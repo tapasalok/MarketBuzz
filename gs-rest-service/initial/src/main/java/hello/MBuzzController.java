360b98fef256d230107adb47e1185d72cbc98cd7
tapasalok <tapasalok@gmail.com>
2018-02-03 15:10:53 +0530

    Commiting

--- gs-rest-service/initial/src/main/java/hello/MBuzzController.java
+++ gs-rest-service/initial/src/main/java/hello/MBuzzController.java
@@ -1,5 +1,7 @@
 package hello;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,12 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MBuzzController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/mbuzz")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="Market Buzz") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
-    }
+	List<MarketTip> facturationSvc = new ArrayList<>();
+
+	@RequestMapping("/mbuzz")
+	public List<MarketTip> greeting(@RequestParam(value = "name", defaultValue = "Market Buzz") String name) {
+
+		facturationSvc
+				.add(new MarketTip(1, "ITC", "Buy", "Short Term", 275f, 270f, 300f, "1 Month", "3rd February 2018"));
+		facturationSvc
+				.add(new MarketTip(2, "IOC", "Buy", "Short Term", 275f, 270f, 300f, "1 Month", "3rd February 2018"));
+		facturationSvc
+				.add(new MarketTip(3, "VEDL", "Buy", "Short Term", 337f, 330f, 390f, "1 Month", "3rd February 2018"));
+		return facturationSvc;
+	}
 }
