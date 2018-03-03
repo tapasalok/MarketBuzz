89f24443b17ad7220941775487d3ced451520cdd
tapasalok <tapasalok@gmail.com>
2018-03-04 02:47:28 +0530

    Tapas Box

--- MarketBuzzApp/src/main/resources/static/js/app/app.js
+++ MarketBuzzApp/src/main/resources/static/js/app/app.js
@@ -3,16 +3,16 @@
 var app = angular.module('crudApp',['ui.router','ngStorage']);
 
 app.constant('urls', {
-	BASE: 'http://ec2-13-127-125-189.ap-south-1.compute.amazonaws.com',
-    MARKET_TIP_SERVICE_API : 'http://ec2-13-127-125-189.ap-south-1.compute.amazonaws.com/api/marketTip/',
-    MARKET_ACTIVE_TIP_SERVICE_API : 'http://ec2-13-127-125-189.ap-south-1.compute.amazonaws.com/api/activeMarketTip/',
+	BASE: 'http://ec2-13-126-203-235.ap-south-1.compute.amazonaws.com',
+    MARKET_TIP_SERVICE_API : 'http://ec2-13-126-203-235.ap-south-1.compute.amazonaws.com/api/marketTip/',
+    MARKET_ACTIVE_TIP_SERVICE_API : 'http://ec2-13-126-203-235.ap-south-1.compute.amazonaws.com/api/activeMarketTip/',
 });
 
 app.config(['$stateProvider', '$urlRouterProvider',
     function($stateProvider, $urlRouterProvider) {
 	    $stateProvider.state('home', {
                 url: '/',
-                templateUrl: '/partials/admin',
+                templateUrl: '/partials/list',
                 controller:'MarketTipController',
                 controllerAs:'ctrl',
                 resolve: {
